e12005cd16f80a73815d7a5a54e5961545fa8493
YADAV VISHAL KUMAR SUSHIL KUMAR <vishal278888@gmail.com>
2026-01-10 19:34:48 +0530

    Add nextPermutation method to Solution class
    Implement next permutation algorithm for an array.

---
+++ Arrays/next_permutation.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int pivot=-1;
+        int n=nums.length;
+        for(int i=n-2; i>=0 ; i--){
+            if(nums[i]<nums[i+1]){
+                pivot=i;
+                break;
+            }
+        }
+        if(pivot==-1){
+           reverse(nums, 0, n-1 );
+           return;
+        }
+
+        for(int i=n-1; i>pivot; i--){
+           if(nums[i]>nums[pivot]){
+            swap(nums, i, pivot);
+            break;
+           } 
+        }
+        reverse(nums, pivot+1, n-1);
+    }
+    private void reverse(int[] nums, int left, int right){
+        while(left<right){
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+    private void swap(int[] nums, int i, int j){
+        int temp= nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
